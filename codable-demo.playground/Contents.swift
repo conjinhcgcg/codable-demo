5142bb7d92183af853ad73309a12b5ab6ab873d8
conjinh <conjinh@capgroup.com>
2018-11-09 16:33:38 -0800

    Adds child object for relationships

--- codable-demo.playground/Contents.swift
+++ codable-demo.playground/Contents.swift
@@ -1,10 +1,11 @@
 import Foundation
 
 final public class Human: NSObject {
-    public let name: String
-    public let language: String
-    public let isMarried: Bool
-    public let limps: Int
+    public var name: String
+    public var language: String
+    public var isMarried: Bool
+    public var limps: Int
+    public var pets: [Pet] = []
     
     public init(name: String, language: String, isMarried: Bool, limps: Int) {
         self.name = name
@@ -16,23 +17,61 @@ final public class Human: NSObject {
 
 extension Human: Codable {
     enum CodingKeys: String, CodingKey {
-        case name = "name"
-        case language = "language"
-        case isMarried = "isMarried"
-        case limps = "limps"
+        case name
+        case language
+        case isMarried
+        case limps
+        case pets
     }
 }
 
 // CustomStringConvertable
 extension Human {
     override public var description: String {
-        return "name: \(self.name), language: \(self.language), isMarried: \(self.isMarried), limp: \(self.limps)"
+        return "name: \(self.name), language: \(self.language), isMarried: \(self.isMarried), limp: \(self.limps), pets: \(self.pets)"
     }
 }
 
+//////////////////////////////////////////////////////////////////////////
 
-// Let's make a human the normal way
-// let man01 = Human(name: "jin", language: "chinese", isMarried: false, limps: 4)
+final public class Pet: NSObject {
+    public weak var owner: Human?
+    public var name: String
+    public let species: String
+    public var age: Int
+    
+    init(name: String, species: String, age: Int) {
+        self.name = name
+        self.species = species
+        self.age = age
+        self.owner = nil
+    }
+}
+
+extension Pet: Codable {
+    enum CodingKeys: String, CodingKey {
+        case name
+        case species
+        case age
+    }
+}
+
+// CustomStringConvertable
+extension Pet {
+    public override var description: String {
+        return "owner: \(String(describing: self.owner?.name)), name: \(self.name), species: \(self.species), age: \(self.age)"
+    }
+}
+
+//////////////////////////////////////////////////////////////////////////
+
+
+// Let's make a human and a pet the normal way
+//let man01 = Human(name: "jin", language: "chinese", isMarried: false, limps: 4)
+//let pet01 = Pet(name: "sponge", species: "cat", age: 1)
+//man01.pets.append(pet01)
+//print(man01)
+
 // Let's mock a json
 let json = """
 [
@@ -40,15 +79,20 @@ let json = """
         "name": "jin",
         "language": "chinese",
         "isMarried": false,
-        "limps": 4
+        "limps": 4,
+        "pets": [
+            {
+                "name": "sponge",
+                "species": "cat",
+                "age": 1
+            }
+        ]
     }
 ]
 """.data(using: .utf8)!
 
-// Let's make a human with Codable
+// Let's make a human and pet with Codable
 let array = try! JSONDecoder().decode([Human].self, from: json)
 for element in array {
     print(element)
 }
-
-print(array.count)
